8b1eb77cdf3edc3c75e3d8a755bb0e8f5502e69a
SunShineOneX <SunShineOneX@gmail.com>
2020-07-12 15:10:19 -0400

    style upd

--- src/App.js
+++ src/App.js
@@ -22,13 +22,30 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <div className = "wrapper">
-          <div className = "form-left">
-            
-          </div>
+        <div className="parent">
+            <div className = "left-form">
+                  <h1>Pawtastic</h1>
+            </div>
+            <div className = "pet-form">
+              <div className = "child-1">
+                <h1>We love pets! Give us the deets about your favorite companion!</h1>
+              </div>
+              <div className = "child-2">
 
+              </div>
+              <div className = "child-3">
+
+              </div>
+              <div className = "child-4">
+
+              </div>
+              <div className = "child-5">
+
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      
     )
   }
 }
